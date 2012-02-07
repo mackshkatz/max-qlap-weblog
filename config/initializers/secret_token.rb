4a872edb044920802a2b6f3c8bd07c9be302591d
Max Katz <maxwelljkatz@gmail.com>
2012-02-06 21:16:16 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaxQlapWeblog::Application.config.secret_token = 'b24b084329bcb14e10d4b5b9ffcb28d28ab511bcf57907631f1ac7e7a5e70b281952cd5846a1a44a6ca468b81d5f563a49bc5332826f8bb7d4c8fc5b27d30c03'
