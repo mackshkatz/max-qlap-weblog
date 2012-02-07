18bda55b2dc176ce2ba247fe658f38aabc27a9db
Max Katz <maxwelljkatz@gmail.com>
2012-02-07 16:01:52 -0700

    Created a shared navigation partial and inlcuded it on the homepage, have condition in navigation partial that shows login for guests, and logout for users.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
 
+	def index
+		
+	end
+	
 	def create
 		user = User.find_by_email(params[:email])
 	  if user && user.authenticate(params[:password])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
 	has_secure_password
-	
+
 	def full_name
 		"#{first_name} #{last_name}"
 	end
