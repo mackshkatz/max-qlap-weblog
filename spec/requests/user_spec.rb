cb5a57e723a1a40f8bde51b066b8c8ab134c48ff
Max Katz <maxwelljkatz@gmail.com>
2012-02-09 13:02:00 -0700

    Realized the integration test I was writing was really a user_spec and not a post_spec

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -13,3 +13,22 @@ describe "A user can log in", :type => :request do
 		page.should have_content "You have been logged in!"
 	end
 end
+
+describe "A guest can't see a certain link" do
+	context "A guest user" do
+		it "should not have the create-a-post page if not logged in" do
+			visit root_path
+			page.should_not have_content "Create-a-post"
+		end
+	end
+
+	context "A user" do
+		it "should have the create-a-post page if logged in" do
+			visit root_path
+			fill_in "email", :with => "willie@springfield.edu"
+			fill_in "password", :with => "hello"
+			click_button "Log in"
+			page.should have_content "Create-a-post"
+		end
+	end
+end
