9e98b7ceb011a46a4d9595dc912ed50e2b606ebc
Max Katz <maxwelljkatz@gmail.com>
2012-02-12 21:59:21 -0700

    Fixed the way I used the before filters. Before I had a before filter requiring the user be logged in on the application controller, so since everything inherited from this I then had to add a skip before filter on the sessions controller for every action. So instead, I just made the before filter on the posts controller and added the except option for the index action, since even guests should be able to still see all the posts that are there

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :require_login
-
   def require_login
     unless logged_in?
       flash[:error] = "You must be logged in to access this section"
