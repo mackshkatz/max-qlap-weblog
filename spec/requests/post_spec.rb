52453368e0dbed11b394e7ed8a43bea25cd7ab6d
Max Katz <maxwelljkatz@gmail.com>
2012-02-12 21:02:48 -0700

    Made the code in my test solely what I am testing, moved the necessary page visits and text input to the before do block

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -3,16 +3,17 @@ require 'spec_helper'
 describe "Blog homepage is displaying post titles" do
 	before do
 		@user = User.create(:first_name => "Groundskeeper", :last_name => "Willie", :email => "willie@springfield.edu", :password => "hello", :password_confirmation => "hello")
-	end
-
-	it "should show what the user just entered" do
 		visit login_path
 		fill_in "email", :with => "willie@springfield.edu"
 		fill_in "password", :with => "hello"
+		click_button "Log in"
 		visit new_post_path
 		fill_in "title", :with => "test title"
 		fill_in "body", :with => "test body"
 		click_button "Create"
+	end
+
+	it "should show what the user just entered" do
 		visit posts_path
 		page.should have_content "test title"
 	end
