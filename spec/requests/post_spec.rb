fba7ccf8e0084b42e812a590588d8948065a4be3
Max Katz <maxwelljkatz@gmail.com>
2012-02-14 10:33:49 -0700

    Added test checking for presence of teaser on blog homepage

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -6,9 +6,9 @@ describe "Blog homepage is displaying post titles" do
 		login(@user, "hello")
 		visit new_post_path
 		fill_in "post_title", :with => "test title"
+		fill_in "post_teaser", :with => "this is the portion of my post I want anyone to be able to see"
 		fill_in "post_body", :with => "test body"
 		click_button "Create"
-		click_link "test title"
 	end
 
 	it "should show what the user just entered" do
@@ -17,6 +17,7 @@ describe "Blog homepage is displaying post titles" do
 	end
 
 	it "should allow user to edit an existing blog post" do
+		click_link "test title"
 		click_link "Edit Post"
 		fill_in "post_title", :with => "updated title"
 		click_button "Update"
@@ -24,9 +25,18 @@ describe "Blog homepage is displaying post titles" do
 	end
 
 	it "should render errors on the page if a user tries to create/update an invalid post" do
+		click_link "test title"
 		click_link "Edit Post"
 		fill_in "post_title", :with => ""
 		click_button "Update"
 		page.should have_content "Title can't be blank"
 	end
+
+	it "Blog homepage shows the teaser for each post" do
+		print page.html
+		page.should have_content "this is the portion of my post I want anyone to be able to see"
+	end
 end
+
+
+
