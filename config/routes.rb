a2c50b0924bdba8fc27ba8849cc01f18d25413e8
Max Katz <maxwelljkatz@gmail.com>
2012-02-14 14:08:04 -0700

    Added a pages controller to handle any static pages, so far it only has an about action for the about me page. Made a simple view for it and added a match route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ MaxQlapWeblog::Application.routes.draw do
   get '/login' => "sessions#new"
 
   resources :posts
+
+  get '/about' => "pages#about"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
