f33f16b5680e5f4f149c905b8747e0c5eb02c337
Max Katz <maxwelljkatz@gmail.com>
2012-02-20 08:31:52 -0700

    Fixed spec, wasn't getting to the proper page before

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -13,6 +13,21 @@ describe "A user can log in", :type => :request do
 		page.should have_content "You have been logged in!"
 		page.should have_content "Create-a-post"
 	end
+
+	it "Slug should be the same as post author's name, instead of post author's id" do
+		visit login_path
+		fill_in "email", :with => "willie@springfield.edu"
+		fill_in "password", :with => "hello"
+		click_button "Log in"
+		click_link "Create-a-post"
+		fill_in "post_title", :with => "test title"
+		fill_in "post_teaser", :with => "this is the portion of my post I want anyone to be able to see"
+		fill_in "post_body", :with => "test body"
+		click_button "Create"
+		click_link "Groundskeeper"
+		current_path.should == "/posts/author/Groundskeeper"
+		page.should have_content "Groundskeeper"
+	end
 end
 
 describe "A guest can't see the create-a-post link" do
@@ -25,16 +40,5 @@ describe "A guest can't see the create-a-post link" do
 			visit root_path
 			page.should_not have_content "Create-a-post"
 		end
-	end
-
-	it "Slug should be the same as post author's name, instead of post author's id" do
-		click_link "Create-a-post"
-		fill_in "post_title", :with => "test title"
-		fill_in "post_teaser", :with => "this is the portion of my post I want anyone to be able to see"
-		fill_in "post_body", :with => "test body"
-		click_button "Create"
-		click_link "Groundskeeper"
-		current_path.should == "/posts/Groundskeeper"
-		page.should have_content "Groundskeeper"
 	end
 end
