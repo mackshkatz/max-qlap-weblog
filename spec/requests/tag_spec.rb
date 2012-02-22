6091bde62c79d9e6c262fb9d696672e4131d2d79
Max Katz <maxwelljkatz@gmail.com>
2012-02-22 11:09:08 -0700

    Tags are now generated as links

--- spec/requests/tag_spec.rb
+++ spec/requests/tag_spec.rb
@@ -8,16 +8,16 @@ describe "Guests can view posts for a given tag" do
 		fill_in "post_title", :with => "test title"
 		fill_in "post_teaser", :with => "this is the portion of my post I want anyone to be able to see"
 		fill_in "post_body", :with => "test body"
-		fill_in "tags", :with => "test tag"
+		fill_in "tags", :with => "test-tag"
 		click_button "Create"
 	end
 
 	it "should show the tags as links on homepage" do
-		page.should have_link "test tag"
+		page.should have_link "test-tag"
 	end
 
 	it "should filter all posts for a given tag" do
-		click_link "test tag"
+		click_link "test-tag"
 		current_path.should == "/posts/tags/test-tag"
 		page.should have_content "test title"
 	end
