bb48b623f4c5b61aa2a3acdb2b3c77ee9192ed8d
Max Katz <maxwelljkatz@gmail.com>
2012-02-24 13:04:24 -0700

    Added tags to posts in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,22 @@
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 max = User.create!(:first_name => "Max", :last_name => "Katz", :password => "hello", :password_confirmation => "hello", :email => "max@quickleft.com")
 kali = User.create!(:first_name => "Kali", :last_name => "Adams", :password => "bye", :password_confirmation => "bye", :email => "kali@quickleft.com")
-
+test_tag = Tag.create(:name => "test")
+test_tag2 = Tag.create(:name => "test2")
 # post1 = max.posts.create(title: "my post title", body: "my body")
 # or
 # post1 = Post.create(user: max, title: "my post title", body: "my body")
 
-post1 = Post.create(:user => max, :title => "first post", :teaser => "read the first one", :body => "We number one yeaaa").tags.create(:name => "test")
-post2 = Post.create(:user => max, :title => "second post", :teaser => "lurk inside", :body => "We number two yeaaa").tags.create(:name => "test")
-post3 = Post.create(:user => max, :title => "third post", :teaser => "attention getter", :body => "We number three yeaaa").tags.create(:name => "shmeh")
-post4 = Post.create(:user => max, :title => "fourth post", :teaser => "gadgdf", :body => "We number four yeaaa").tags.create(:name => "test")
-post5 = Post.create(:user => kali, :title => "fifth post", :teaser => "read the first one", :body => "We number five yeaaa").tags.create(:name => "test")
-post6 = Post.create(:user => kali, :title => "sixth post", :teaser => "read the first one", :body => "We number six yeaaa").tags.create(:name => "two")
-post7 = Post.create(:user => max, :title => "seventh post", :teaser => "read the first one", :body => "We number seven yeaaa").tags.create(:name => "two")
-post8 = Post.create(:user => max, :title => "eighth post", :teaser => "read the first one", :body => "We number eight yeaaa").tags.create(:name => "test")
-post9 = Post.create(:user => max, :title => "ninth post", :teaser => "sgadsg", :body => "We number nine yeaaa").tags.create(:name => "shmeh")
-post10 = Post.create(:user => kali, :title => "tenth post", :teaser => "read the last one", :body => "We number ten yeaaa").tags.create(:name => "two")
+post1 = Post.create(:user => max, :title => "first post", :teaser => "read the first one", :body => "We number one yeaaa")
+post2 = Post.create(:user => max, :title => "second post", :teaser => "lurk inside", :body => "We number two yeaaa")
+post2.tags << test_tag << test_tag2
+post3 = Post.create(:user => max, :title => "third post", :teaser => "attention getter", :body => "We number three yeaaa")
+post3.tags << test_tag
+post4 = Post.create(:user => max, :title => "fourth post", :teaser => "gadgdf", :body => "We number four yeaaa")
+post4.tags << test_tag
+post5 = Post.create(:user => kali, :title => "fifth post", :teaser => "read the first one", :body => "We number five yeaaa")
+post6 = Post.create(:user => kali, :title => "sixth post", :teaser => "read the first one", :body => "We number six yeaaa")
+post7 = Post.create(:user => max, :title => "seventh post", :teaser => "read the first one", :body => "We number seven yeaaa")
+post8 = Post.create(:user => max, :title => "eighth post", :teaser => "read the first one", :body => "We number eight yeaaa")
+post9 = Post.create(:user => max, :title => "ninth post", :teaser => "sgadsg", :body => "We number nine yeaaa")
+post10 = Post.create(:user => kali, :title => "tenth post", :teaser => "read the last one", :body => "We number ten yeaaa")
