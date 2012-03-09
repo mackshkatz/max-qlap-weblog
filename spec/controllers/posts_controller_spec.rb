91601462b170afe1877b416fe9b5501aa7207a2b
Max Katz <maxwelljkatz@gmail.com>
2012-03-09 14:01:46 -0700

    Added spec for presence of rss feed

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe PostsController do
-
+	describe "GET 'RSS Feed'" do
+		it "should be successful" do
+			get 'index', :format => 'rss'
+			response.should be_success
+		end
+	end
 end
