57b9e1607a144d11deb64941c75dd70761641502
Max Katz <maxwelljkatz@gmail.com>
2012-03-13 13:38:49 -0600

    Specified host name in carrier wave config, images now successfully upload to S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,10 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',       # required
     :aws_access_key_id      => ENV['S3_KEY'],       # required
     :aws_secret_access_key  => ENV['S3_SECRET'],       # required
-    :region                 => 'us-standard'  # optional, defaults to 'us-east-1'
+    #:region                 => 'us-standard'  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'QLAP-weblog'                     # required (bucket name)
-  # config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
+  config.fog_host       = '//QLAP-weblog.s3.amazonaws.com'            # optional, defaults to nil
   config.fog_public     = false                                   # optional, defaults to true
   # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 
