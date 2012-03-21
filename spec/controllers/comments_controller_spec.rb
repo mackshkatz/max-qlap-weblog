874ac0c3e7ec4a9d56d29a3c13c9fd5d5289ccbe
Max Katz <maxwelljkatz@gmail.com>
2012-03-21 15:44:00 -0600

    Finished final comments controller spec, used proper naming convention for request specs

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -6,13 +6,20 @@ describe CommentsController do
 		@comment1 = Comment.create(:body => 'one', :email => 'test@test.com' )
 		@comment2 = Comment.create(:body => 'two', :email => 'test@test.com' )
 		@comment3 = Comment.create(:body => 'three', :email => 'test@test.com' )
-		@comments = [ @comment1, @comment2, @comment3 ]
+		# @comments = [ @comment1, @comment2, @comment3 ]
 	end
 
 	describe "When getting the index" do
 		it "lists out all the comments pending approval" do
 			get :index
 			assigns(:comments).count.should == 3
+		end
+	end
+
+	describe "When approving a comment" do
+		it "changed the approved attribute to true" do
+			put :update, :id => 1
+			Comment.find(1).approved.should == true
 		end
 	end
 end
