d643bdff94ec07b0d41faf2759a4168e8ef8d3b8
Max Katz <maxwelljkatz@gmail.com>
2012-04-03 14:44:43 -0600

    Refactored code so it would make new request after the previous one has been completed, rather than just firing every 10 seconds

--- app/assets/javascripts/twitter_stream.js
+++ app/assets/javascripts/twitter_stream.js
@@ -8,19 +8,27 @@ var twitter_stream = {
 	startStream: function(e) {
 		e.preventDefault();
 		user_search_input = $('input[type="text"]').val();
-		twitter_search = "http://search.twitter.com/search.json?q=" + encodeURIComponent(user_search_input)
-		setInterval(function() {
+		twitter_search = "http://search.twitter.com/search.json?q=" + encodeURIComponent(user_search_input);
+		
+		var getTweets = function() {
 			$.ajax({
 				url: twitter_search,
 				success: function(data) {
-
+					console.log(data);
+					var new_tweet_text = data.results[0].text;
+					var new_tweet_user = data.results[0].from_user;
+					$('.tweet-stream').after('<div><p><span>' + new_tweet_user + ':</span>' + new_tweet_text + '</p></div>');
 				},
-				dataType: "jsonp"
+				dataType: "jsonp",
+				complete: function() {
+					setTimeout(getTweets, 1000);
+				}
 			});	
-		}, 10000);
+		};
+		getTweets();
 	}
 }
 
-$('document').ready(function() {
+$(document).ready(function() {
 	twitter_stream.initialize();
 });
