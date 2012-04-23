b64caed493aaeecd56c7d7cb4558a83016349c85
Max Katz <maxwelljkatz@gmail.com>
2012-04-23 15:10:18 -0600

    Conditions for each cell are now done, and is redrawing based on nextRound life status, logic for determining life or not for the next round may need to be fixed a bit though it seems

--- app/assets/javascripts/game_of_life.js
+++ app/assets/javascripts/game_of_life.js
@@ -4,6 +4,7 @@
 	Cell.prototype.initialize = function(i, j, current_column) {
 		this.column = i;
 		this.row = j;
+		this.cell_number = parseInt(i + "" + j);
 		this.DOMNode = $('<div id="' + i + j + '" class="cell"></div>').appendTo(current_column);
 	}
 
@@ -24,10 +25,11 @@
 
 		generateGrid: function() {
 			window.grid = [];
-			var x_value = $('.x-value').val();
-			var y_value = $('.y-value').val();
+			window.x_value = $('.x-value').val();
+			window.y_value = $('.y-value').val();
+			var matrix_grid = $('.grid');
 			for (var i = 0; i < x_value; i++) {
-				var current_column = $('<div class="column"></div>').appendTo('.grid');
+				var current_column = $('<div class="column"></div>').appendTo(matrix_grid);
 				for (var j = 0; j < y_value; j++) {
 					var current_cell = new Cell();
 					current_cell.initialize(i, j, current_column);
@@ -44,26 +46,59 @@
 
 		startLife: function() {
 			setInterval(app.cycle, 100);
+			// app.cycle();
 			//loop through each cell in grid array, checking if it's on and the total score of the cells around it and determine
 			//if it should be on or off in next state, and push that into window.new_grid
 		},
 
 		cycle: function() {
-			this.generateNextGrid();
-			this.drawGrid();
+			app.generateNextGrid();
+			app.drawGrid();
 		},
 
 		generateNextGrid: function() {
 			for (var i = 0; i < grid.length; i++) {
-				var current_cell = grid[i];
-				if (current_cell.DOMNode.hasClass('on')) {
-					
+				var score = 0;
+				// cells to the left
+				if ((grid[i - y_value - 1]) && (!(grid[i].cell_number % y_value == 0)) && (grid[i - y_value - 1].DOMNode.hasClass('on'))) score++;
+				if ((grid[i - y_value]) && (grid[i - y_value].DOMNode.hasClass('on'))) score++;
+				if ((grid[i - y_value + 1]) && (!(grid[i].cell_number % (y_value - 1) == 0)) && (grid[i - y_value + 1].DOMNode.hasClass('on'))) score++;
+
+				// cells above and below
+				if ((!(grid[i].cell_number % y_value == 0)) && (grid[i - 1].DOMNode.hasClass('on'))) score++;
+				if ((!(grid[i].cell_number % (y_value - 1) == 0)) && (grid[i + 1].DOMNode.hasClass('on'))) score++;
+
+				// cells to the right
+				if ((grid[i + y_value - 1]) && (!(grid[i].cell_number % y_value == 0)) && (grid[i + y_value - 1].DOMNode.hasClass('on'))) score++;
+				if ((grid[i + y_value]) && (grid[i + y_value].DOMNode.hasClass('on'))) score++;
+				if ((grid[i + y_value + 1]) && (!(grid[i].cell_number % (y_value - 1) == 0)) && (grid[i + y_value + 1].DOMNode.hasClass('on'))) score++;				
+				// console.log("cell", i, ":", score);
+
+				if (grid[i].DOMNode.hasClass('on') && (score < 2)) {
+					grid[i].nextRound = 0;
+				} else if (grid[i].DOMNode.hasClass('on') && (score == 2 || 3)) {
+					grid[i].nextRound = 1;
+				} else if (grid[i].DOMNode.hasClass('on') && (score > 3)) {
+					grid[i].nextRound = 0;
+				} else if (!(grid[i].DOMNode.hasClass('on')) && (score == 3)) {
+					grid[i].nextRound = 1;
+				} else if (!(grid[i].DOMNode.hasClass('on')) && (score !== 3)) {
+					grid[i].nextRound = 0;
 				}
+
+				// console.log(grid[i].nextRound);
 			}
 		},
-
+		generation: 0,
 		drawGrid: function() {
-
+			for (var i = 0; i < grid.length; i++) {
+				if ((grid[i].DOMNode.hasClass('on')) && (grid[i].nextRound == 0)) {
+					grid[i].DOMNode.removeClass('on');
+				} else if ((!(grid[i].DOMNode.hasClass('on'))) && (grid[i].nextRound == 1)) {
+					grid[i].DOMNode.addClass('on');
+				}
+			}
+			console.log("generation:", app.generation)
 		}
 	}
 	
